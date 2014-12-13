29d4539317cd92ffe95ad8acbd385ca1530b0ab8
ariel <skuratovichs@student.mini.pw.edu.pl>
2014-12-13 16:34:11 +0100

    ptrace debugger detection for linux x86/x64

---
+++ dDeflect/data/os/linux/x64/ptrace.asm
@@ -0,0 +1,32 @@
+[bits 64]
+
+; save registers
+push rax
+push rdi
+push rsi
+push rdx
+push r10
+
+mov rax, 101 ; syscall ptrace
+; ptrace parametres
+mov rdi, 0 ; PTRACE_TRACEME
+mov rsi, 0
+mov rdx, 1
+mov r10, 0
+
+syscall ; syscall
+
+; rax == return value of ptrace
+; use a wrapper for return value
+cmp rax, 0
+jge old_stack
+mov rax, 60; exit syscall
+mov ebx, 127; error code
+syscall ; syscall
+
+old_stack:
+pop r10
+pop rdx
+pop rsi
+pop rdi
+pop rax
