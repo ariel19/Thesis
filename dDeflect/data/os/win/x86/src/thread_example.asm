c0355cb8471550db99e14f5eeee066668e372896
Mateusz Jabłoński <mjablonski92@gmail.com>
2014-12-23 20:32:31 +0100

    thread example

--- dDeflect/data/os/win/x86/src/thread_example.asm
+++ dDeflect/data/os/win/x86/src/thread_example.asm
@@ -1,39 +1,4 @@
 [bits 32]
-
-		push 	edx					; GetProcAddr
-		push 	eax					; LoadLibrary
-		
-		sub		esp, 16
-		
-		call 	xxx
-		db 		"user32.dll", 0x00
-xxx:
-		call	[ebp - 8]			; LoadLibrary
-		mov		[ebp - 16], eax
-		
-		call 	yyy
-		db		"kernel32.dll", 0x00
-yyy:
-		call	[ebp - 8]			; LoadLibrary
-		mov		[ebp - 12], eax
-		
-		call	zzz
-		db		"MessageBoxA", 0x00
-zzz:
-		push	DWORD [ebp - 16]	; user32.dll
-		call	[ebp - 4]			; GetProcAddr
-		mov		[ebp - 20], eax
-		
-		call	vvv
-		db		"Sleep", 0x00
-vvv:
-		push	DWORD [ebp - 12]	; kernel32.dll
-		call	[ebp - 4]			; GetProcAddr
-		mov		[ebp - 24], eax
-		
-loopt:
-		push	10000
-		call	[ebp - 24]			; Sleep(10000)
 		
 		push	0x00
 		push	0x00
@@ -41,7 +6,6 @@ loopt:
 		db		":)", 0x00
 ttt:
 		push	0x00
-		call	[ebp - 20]			; MessageBoxA
+		call	eax
 		
-		jmp		loopt
 		
