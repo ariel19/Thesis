1d6ed01db40b7b90b83ca0b325c0032b3138f3fe
ariel19 <skuratovichs@student.mini.pw.edu.pl>
2014-12-23 14:54:31 +0100

    interface to secure elf files

--- dDeflect/src/core/adding_methods/wrappers/linux/daddingmethods.h
+++ dDeflect/src/core/adding_methods/wrappers/linux/daddingmethods.h
@@ -5,6 +5,8 @@
 #include <QMap>
 #include <QString>
 
+#include <core/file_types/elffile.h>
+
 class DAddingMethods {
 
 public:
@@ -18,7 +20,7 @@ public:
     };
 
     /**
-     * @brief Rejestry dla architektury x86
+     * @brief Rejestry dla architektury x86.
      */
     enum class Registers_x86 {
         EAX,
@@ -32,7 +34,7 @@ public:
     };
 
     /**
-     * @brief Rejestry dla architektury x64
+     * @brief Rejestry dla architektury x64.
      */
     enum class Registers_x64 {
         RAX,
@@ -53,6 +55,9 @@ public:
         R15
     };
 
+    /**
+     * @brief Klasa bazowa reprezentująca opakowanie dla kawałków kodu.
+     */
     template <typename RegistersType>
     class Wrapper {
         QList<RegistersType> used_regs;
@@ -61,23 +66,53 @@ public:
         QString code;
     };
 
+    /**
+     * @brief Klasa reprezentująca opakowanie dla tworzenia nowego wątku.
+     */
     template <typename RegistersType>
     class ThreadWrapper : public Wrapper<RegistersType> {
         Wrapper<RegistersType> *thread_action;
     };
 
+    /**
+     * @brief Klasa reprezentująca opakowanie dla tworzenia nowego punktu wejściowego.
+     */
     template <typename RegistersType>
     class OEPWrapper : public Wrapper<RegistersType> {
         Wrapper<RegistersType> *oep_action;
     };
 
+
+    /**
+     * @brief Klasa reprezentująca opakowanie dla tworzenia tramplin w funkcjach bibliotecznych.
+     */
     template <typename RegistersType>
     class TrampolineWrapper : public Wrapper<RegistersType> {
 
     };
 
+    /**
+     * @brief Klasa opisująca metodę wstrzykiwania kodu.
+     */
+    template <typename RegistersType>
+    class InjectDescription {
+        CallingMethod cm;
+        Wrapper<RegistersType> *adding_method;
+    };
 
+    /**
+     * @brief Konstruktor.
+     */
     DAddingMethods() {}
+
+    /**
+     * @brief Metoda zabezpiecza plik, podany jako argument za pomocą wyspecyfikowanej metody.
+     * @param elf plik do zabezpieczania.
+     * @param inject_desc opis metody wstrzykiwania kodu.
+     * @return True, jeżeli operacja się powiodła, False w innych przypadkach.
+     */
+    template <typename RegistersType>
+    bool secure_elf(ELF &elf, const InjectDescription<RegistersType> &inject_desc);
 };
 
 #endif // DADDINGMETHODS_H
