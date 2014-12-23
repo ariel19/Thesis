12de104f01bf9330544e0a8ab8e7f5bd072477ab
ariel19 <skuratovichs@student.mini.pw.edu.pl>
2014-12-23 14:56:15 +0100

    asm code generator helper class

---
+++ dDeflect/src/core/adding_methods/wrappers/linux/asmcodegenerator.cpp
@@ -0,0 +1,5 @@
+#include "asmcodegenerator.h"
+
+AsmCodeGenerator::AsmCodeGenerator()
+{
+}
---
+++ dDeflect/src/core/adding_methods/wrappers/linux/asmcodegenerator.h
@@ -0,0 +1,10 @@
+#ifndef ASMCODEGENERATOR_H
+#define ASMCODEGENERATOR_H
+
+class AsmCodeGenerator
+{
+public:
+    AsmCodeGenerator();
+};
+
+#endif // ASMCODEGENERATOR_H
