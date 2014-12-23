d5faf01c672082c1dfa7a32db4353769e1f6957c
jakub-kaluza <jakub.kaluza@yahoo.com>
2014-12-23 16:49:00 +0100

    -

---
+++ dDeflect/src/DSourceCodeParser/dsourcecodeparser.cpp
@@ -0,0 +1,5 @@
+#include "dsourcecodeparser.h"
+
+DSourceCodeParser::DSourceCodeParser()
+{
+}
---
+++ dDeflect/src/DSourceCodeParser/dsourcecodeparser.h
@@ -0,0 +1,10 @@
+#ifndef DSOURCECODEPARSER_H
+#define DSOURCECODEPARSER_H
+
+class DSourceCodeParser
+{
+public:
+    DSourceCodeParser();
+};
+
+#endif // DSOURCECODEPARSER_H
