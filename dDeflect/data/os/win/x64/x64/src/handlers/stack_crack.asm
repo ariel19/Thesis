7e33eedbd1ecad0124771690dc79ca47eb687de7
Mateusz Jabłoński <mjablonski92@gmail.com>
2014-12-24 19:20:59 +0100

    x64 shadow space fix

--- dDeflect/data/os/win/x64/x64/src/handlers/stack_crack.asm
+++ dDeflect/data/os/win/x64/x64/src/handlers/stack_crack.asm
@@ -6,4 +6,12 @@
 			xor		[rsp + 8], rsi
 			or		[rsp + 16], rdi
 			and		[rsp + 24], rsp
+			xor		[rsp + 28], rdx
+			xor		[rsp + 32], rsi
+			or		[rsp + 36], rdi
+			and		[rsp + 40], rsp
+			xor		[rsp + 44], rdx
+			xor		[rsp + 48], rsi
+			or		[rsp + 52], rdi
+			and		[rsp + 56], rsp
 			
