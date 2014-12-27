065ad95372711096d9d197d023149efa7ef567f8
jablonskim <mjablonski92@gmail.com>
2014-12-27 20:56:40 +0100

    x64 init

--- dDeflect/tests/src/main.cpp
+++ dDeflect/tests/src/main.cpp
@@ -174,7 +174,7 @@
 
 int main()
 {
-    QFile f("C:\\Users\\jablonskim\\Desktop\\Programy\\putty.exe");
+    QFile f("C:\\Users\\jablonskim\\Desktop\\Project1.exe");
     if(!f.open(QFile::ReadOnly))
         return 1;
 
@@ -188,8 +188,8 @@ int main()
         return 1;
     }
 
-    QList<InjectDescription<Register_x86>*> ids;
-    ids.append(new (std::nothrow) InjectDescription<Register_x86>(CallingMethod::EntryPoint, Wrapper<Register_x86>::fromFile(Wrapper<Register_x86>::methodsPath + "create_thread.asm", true)));
+    QList<InjectDescription<Register_x64>*> ids;
+    ids.append(new (std::nothrow) InjectDescription<Register_x64>(CallingMethod::EntryPoint, Wrapper<Register_x64>::fromFile(Wrapper<Register_x64>::methodsPath + "create_thread.asm", true)));
     if(pe.injectCode(ids))
         puts("Success!");
     else
